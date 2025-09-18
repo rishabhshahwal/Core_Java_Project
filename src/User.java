720e252c1fef4585a28b1eef2c9ed1a599086b32
Rishabh Kumar Shahwal <rishabhshahwal03@gmail.com>
2025-09-18 22:38:47 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -4,47 +4,57 @@ public class User {
     private String fullName;
     private String contact;
 
-    public User(String username, String password, String fullName, String contact) {
+    public User(String username, String password, String fullName, String contact) 
+    {
         this.username = username;
         this.password = password;
         this.fullName = fullName;
         this.contact = contact;
     }
 
-    public String getUsername() {
+    public String getUsername() 
+    {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(String username) 
+    {
         this.username = username;
     }
 
-    public String getPassword() {
+    public String getPassword() 
+    {
         return password;
     }
 
-    public void setPassword(String password) {
+    public void setPassword(String password) 
+    {
         this.password = password;
     }
 
-    public String getFullName() {
+    public String getFullName() 
+    {
         return fullName;
     }
 
-    public void setFullName(String fullName) {
+    public void setFullName(String fullName) 
+    {
         this.fullName = fullName;
     }
 
-    public String getContact() {
+    public String getContact() 
+    {
         return contact;
     }
 
-    public void setContact(String contact) {
+    public void setContact(String contact) 
+    {
         this.contact = contact;
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return fullName + " ("+username+")";
     }
 }
