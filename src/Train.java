e0be71bfc2fbc68e3b234d502ec2c9c716df7d0f
Rishabh Kumar Shahwal <rishabhshahwal03@gmail.com>
2025-09-18 22:40:58 +0530

    Update Train.java

--- src/Train.java
+++ src/Train.java
@@ -1,4 +1,5 @@
-public class Train {
+public class Train 
+{
     private int trainId;
     private String name;
     private String source;
@@ -8,7 +9,8 @@ public class Train {
 
     private int availableSeats;
 
-    public Train(int trainId, String name, String source, String destination, int totalSeats) {
+    public Train(int trainId, String name, String source, String destination, int totalSeats) 
+    {
         this.trainId = trainId;
         this.name = name;
         this.source = source;
@@ -17,51 +19,63 @@ public class Train {
         this.availableSeats = totalSeats;
     }
 
-    public int getTrainId() {
+    public int getTrainId() 
+    {
         return trainId;
     }
 
-    public void setTrainId(int trainId) {
+    public void setTrainId(int trainId) 
+    {
         this.trainId = trainId;
     }
 
-    public String getName() {
+    public String getName() 
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) 
+    {
         this.name = name;
     }
 
-    public String getSource() {
+    public String getSource() 
+    {
         return source;
     }
 
-    public void setSource(String source) {
+    public void setSource(String source) 
+    {
         this.source = source;
     }
 
-    public String getDestination() {
+    public String getDestination() 
+    {
         return destination;
     }
 
-    public void setDestination(String destination) {
+    public void setDestination(String destination) 
+    {
         this.destination = destination;
     }
 
-    public int getTotalSeats() {
+    public int getTotalSeats() 
+    {
         return totalSeats;
     }
 
-    public void setTotalSeats(int totalSeats) {
+    public void setTotalSeats(int totalSeats) 
+    {
         this.totalSeats = totalSeats;
     }
 
-    public int getAvailableSeats() {
+    public int getAvailableSeats() 
+    {
         return availableSeats;
     }
 
-    public void setAvailableSeats(int availableSeats) {
+    public void setAvailableSeats(int availableSeats) 
+    {
         this.availableSeats = availableSeats;
     }
 
@@ -81,7 +95,8 @@ public class Train {
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return trainId + " | "+name+" | "+source+ " -> "+destination+" Seats Available: "+availableSeats;
     }
 }
